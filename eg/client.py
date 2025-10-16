3e5a0d3bfd1ed17f6f82e3aa24cb23d24e643099
Peter Newstein <peternewstein@gmail.com>
2025-10-16 13:27:04 -0700

    :fix added progress updates to example client

--- eg/client.py
+++ eg/client.py
@@ -1,6 +1,7 @@
 import logging
 import sys
 from pathlib import Path
+from typing import Generator
 
 from qtpy.QtWidgets import (
     QVBoxLayout,
@@ -20,12 +21,12 @@ from qt_remote_commands_over_ssh_for_napari_plugins import (
     to_string,
 )
 
-from request import Request
 import napari
 import napari.viewer
 from napari.layers import Image
-from napari.qt.threading import thread_worker, FunctionWorker
+from napari.qt.threading import thread_worker, GeneratorWorker
 import numpy as np
+from request import Request
 
 logging.basicConfig(
     filename="client.log",
@@ -75,6 +76,8 @@ class RemoteGamma(QWidget):
         self.submit_button = QPushButton("Submit Coords")
         self.submit_button.clicked.connect(self.submit)
         layout.addWidget(self.submit_button)
+        self.status = QLabel("")
+        layout.addWidget(self.status)
 
     def reset_image_box(self):
         """
@@ -91,8 +94,14 @@ class RemoteGamma(QWidget):
         if old_value in values:
             self.image_box.setCurrentText(old_value)
 
+    def update_status(self, text: str):
+        """
+        updates the status bar. takes yielded values
+        """
+        self.status.setText(text)
+
     @thread_worker
-    def submit_thread(self) -> dict:
+    def submit_thread(self) -> Generator[str, None, dict]:
         """
         Submits a request to the server
         returns data to be passed as kwargs to napari.Viewer.add_image
@@ -102,19 +111,23 @@ class RemoteGamma(QWidget):
         data = input_layer.data
 
         local_path = Path("data.npy")
+        yield "saving localy"
         np.save(local_path, data)
         try:
             # Acquire connection and send data to remote server
             with self.cm as client:
+                yield "sending file"
                 client.send_file(local_path)
                 # Send processing request with gamma parameter
                 request = Request(str(local_path), float(self.gamma.text()))
+                yield "processing data"
                 response = client.request(to_string(request))
                 if response.error:
                     error_callback(response.error)
                     raise RuntimeError(response.error)
                 # Download processed result from remote server
                 file = Path(response.out)
+                yield "receiving file"
                 client.receive_file(file, file)
             data = np.load(file)
             file.unlink()
@@ -132,9 +145,15 @@ class RemoteGamma(QWidget):
         """
         turn back on the submit button and add image
         """
+        self.status.setText("Done")
+        viewer.add_image(**add_image_kwargs)
+
+    def reset_button(self):
+        """
+        Cleans up the button
+        """
         self.submit_button.setChecked(False)
         self.submit_button.setEnabled(True)
-        viewer.add_image(**add_image_kwargs)
 
     def submit(self, *args):
         _ = args
@@ -142,9 +161,13 @@ class RemoteGamma(QWidget):
         self.submit_button.setChecked(True)
         self.submit_button.setEnabled(False)
         # Start processing in background thread to keep UI responsive
-        worker: FunctionWorker = self.submit_thread()  # type: ignore
+        worker: GeneratorWorker = self.submit_thread()  # type: ignore
+        # add the progress indicators
+        worker.yielded.connect(self.update_status)
         # Add result as new layer when processing completes
         worker.returned.connect(self.post_submit)
+        # Add result as new layer when processing completes
+        worker.finished.connect(self.reset_button) # type: ignore
         # Re-raise exceptions on main thread for visibility
         worker.errored.connect(raise_exception)
         worker.start()
